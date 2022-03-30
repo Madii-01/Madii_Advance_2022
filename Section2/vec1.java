1e0f216bd5c715ac83d047e2eac8e0ac7cece492
Abed Al Qader Madi <80624286+Madii-01@users.noreply.github.com>
2022-03-30 16:29:34 +0300

    Madi

---
+++ Section2/vec1.java
@@ -0,0 +1,51 @@
+package Section2;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Vector;
+
+public class vec1 {
+    public static void main(String[] args) {
+        
+        Vector vec1 = new Vector<String>(4);
+
+        Vector vec2 = new Vector<String>(4);
+
+        vec1.addElement("F");
+        vec1.addElement("W");
+        vec1.addElement("Q");
+        vec1.addElement("F");
+
+        vec2.addElement("A");
+        vec2.addElement("V");
+        vec2.addElement("S");
+        vec2.addElement("B");
+
+        vec1.addAll(0,vec2);
+
+    System.out.println(vec1);
+
+    if(vec1.contains("W")){
+
+        System.out.println("W still exists in the index of " + vec1.indexOf("W"));
+    }else{
+        System.out.println("W does not exist");
+    }
+
+    Collections.sort(vec1);
+    System.out.println(vec1);
+
+
+    List<String> subList = vec1.subList(1 , 5);
+
+    System.out.println("the sublist is: "+subList);
+
+    System.out.println("is vec1 equal to vec2 ? " +vec1.equals(vec2));
+
+
+
+
+    }
+
+    
+}
