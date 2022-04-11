0268cc159a5b0479dfc8c2b21aaa2edd05c24088
Abed Al Qader Madi <80624286+Madii-01@users.noreply.github.com>
2022-04-11 14:57:58 +0300

    madi

--- GenericClass.java
+++ GenericClass.java
@@ -12,19 +12,4 @@ public class GenericClass <E> {
         this.value = value;
     }
     
-    public static void main(String[] args) {
-        GenericClass<String> x = new GenericClass<String>();
-
-        GenericClass<Integer> y = new GenericClass<Integer>();
-
-
-        x.setValue("Madiii");
-        y.setValue(12345);
-
-
-        System.out.println(x.getValue());
-        System.out.println(y.getValue());
-
-
-    }
-}
+    
--- TestGenericClass.java
+++ TestGenericClass.java
@@ -1,7 +1,18 @@
 public class TestGenericClass {
     public static void main(String[] args) {
         
+        GenericClass<String> x = new GenericClass<String>();
 
-        
+        GenericClass<Integer> y = new GenericClass<Integer>();
+
+
+        x.setValue("Madiii");
+        y.setValue(12345);
+
+
+        System.out.println(x.getValue());
+        System.out.println(y.getValue());
+
+
     }
 }
